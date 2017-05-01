d035e10584a1804e4460ce971862f76966727357
edusalguero <edusalguero@gmail.com>
2017-05-01 23:25:07 +0200

    Harvest status and machine status enumerators

---
+++ src/main/java/com/edusalguero/rexoubador/domain/HarvestStatus.java
@@ -0,0 +1,10 @@
+package com.edusalguero.rexoubador.domain;
+
+
+public enum HarvestStatus {
+    IN_PROGRESS,
+    CONNECTION_ERROR,
+    DONE,
+    PENDING
+}
+
---
+++ src/main/java/com/edusalguero/rexoubador/domain/MachineStatus.java
@@ -0,0 +1,9 @@
+package com.edusalguero.rexoubador.domain;
+
+
+public enum MachineStatus {
+    UP,
+    DOWN,
+    UNKNOWN
+}
+
