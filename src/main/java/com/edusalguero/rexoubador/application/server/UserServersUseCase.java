21ae851319e4f6ae99a70785bab71ff854fa0ea2
edusalguero <edusalguero@gmail.com>
2017-05-02 19:05:49 +0200

    Fix typos

--- src/main/java/com/edusalguero/rexoubador/application/server/UserServersUseCase.java
+++ src/main/java/com/edusalguero/rexoubador/application/server/UserServersUseCase.java
@@ -1,6 +1,5 @@
 package com.edusalguero.rexoubador.application.server;
 
-import com.edusalguero.rexoubador.application.contact.ContactResponse;
 import com.edusalguero.rexoubador.domain.server.Server;
 import com.edusalguero.rexoubador.domain.user.User;
 import com.edusalguero.rexoubador.domain.user.UserId;
@@ -20,8 +19,8 @@ public class UserServersUseCase {
     public ArrayList<ServerResponse> execute(UserId userId) {
         ArrayList<ServerResponse> userServers = new ArrayList<>();
         User user = userRepository.ofId(userId);
-        List<Server> contacts = user.servers();
-        for (Server server : contacts) {
+        List<Server> servers = user.servers();
+        for (Server server : servers) {
             userServers.add(new ServerResponse(server));
         }
         return userServers;
