b8ca67d4aeceac2710b5558d110cae54c4564724
edusalguero <edusalguero@gmail.com>
2017-05-13 10:14:27 +0200

    Rename use case

--- src/main/java/com/edusalguero/rexoubador/application/monitor/HarvestServersMonitoringDataUseCase.java
+++ src/main/java/com/edusalguero/rexoubador/application/monitor/HarvestServersMonitoringDataUseCase.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 
 @Service
-public class CollectServersDataUseCase {
+public class HarvestServersMonitoringDataUseCase {
 
     @Autowired
     private ServerRepository serverRepository;
