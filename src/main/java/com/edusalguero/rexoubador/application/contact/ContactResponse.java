40fb43e2eda7cc129514f78b9ad7a2aabbbd3e64
edusalguero <edusalguero@gmail.com>
2017-05-19 23:11:48 +0200

    Add slack DTO to map slack data

--- src/main/java/com/edusalguero/rexoubador/application/contact/ContactResponse.java
+++ src/main/java/com/edusalguero/rexoubador/application/contact/ContactResponse.java
@@ -5,27 +5,19 @@ import com.edusalguero.rexoubador.domain.model.contact.Contact;
 import com.edusalguero.rexoubador.domain.shared.Status;
 
 import java.util.Date;
-import java.util.HashMap;
 
 
 public class ContactResponse {
     private String id;
     private String email;
-
-    private String slackWebhookUrl;
-
-    private String slackChannelOrUsername;
-
-
     private Date entryDate;
-
     private Status status;
+    private Slack slack;
 
     public ContactResponse(Contact contact) {
         this.id = contact.id();
         this.email = contact.email();
-        this.slackChannelOrUsername = contact.slackChannelOrUsername();
-        this.slackWebhookUrl = contact.slackWebhookUrl();
+        this.slack = new Slack(contact.slackChannelOrUsername(), contact.slackWebhookUrl());
         this.entryDate = contact.entryDate();
         this.status = contact.status();
     }
@@ -38,13 +30,9 @@ public class ContactResponse {
         return email;
     }
 
-    public HashMap<String, String> getSlack() {
-        HashMap<String, String> slack = new HashMap<>();
-        slack.put("channelOrUsername", slackChannelOrUsername);
-        slack.put("webhookUrl", slackWebhookUrl);
+    public Slack getSlack() {
         return slack;
     }
-
 
     public String getEntryDate() {
         return DateConverter.getFormattedDateOrEmptyString(entryDate);
@@ -52,5 +40,23 @@ public class ContactResponse {
 
     public Status getStatus() {
         return status;
+    }
+
+    class Slack {
+        private String slackWebhookUrl;
+        private String slackChannelOrUsername;
+
+        Slack(String slackWebhookUrl, String slackChannelOrUsername) {
+            this.slackWebhookUrl = slackWebhookUrl;
+            this.slackChannelOrUsername = slackChannelOrUsername;
+        }
+
+        public String getSlackWebhookUrl() {
+            return slackWebhookUrl;
+        }
+
+        public String getSlackChannelOrUsername() {
+            return slackChannelOrUsername;
+        }
     }
 }
