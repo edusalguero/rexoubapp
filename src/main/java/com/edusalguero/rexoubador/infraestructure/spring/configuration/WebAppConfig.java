6a0c8c42bf35e356aa09f6f25dcaf6a683f626ec
edusalguero <edusalguero@gmail.com>
2017-05-20 17:18:10 +0200

    Added swagger-ui as resource

---
+++ src/main/java/com/edusalguero/rexoubador/infraestructure/spring/configuration/WebAppConfig.java
@@ -0,0 +1,19 @@
+package com.edusalguero.rexoubador.infraestructure.spring.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+}
