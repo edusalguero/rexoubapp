cbf67b9a3da5d11365db31c4c3bad44793cf0483
edusalguero <edusalguero@gmail.com>
2017-05-25 23:55:04 +0200

    Add available harvesters to user when sign up

--- src/main/java/com/edusalguero/rexoubador/application/user/UserRegistrationUseCase.java
+++ src/main/java/com/edusalguero/rexoubador/application/user/UserRegistrationUseCase.java
@@ -1,37 +1,56 @@
 package com.edusalguero.rexoubador.application.user;
 
+import com.edusalguero.rexoubador.domain.model.monitor.harvester.HarvesterRepository;
+import com.edusalguero.rexoubador.domain.model.monitor.harvester.HarvesterType;
 import com.edusalguero.rexoubador.domain.model.user.User;
 import com.edusalguero.rexoubador.domain.model.user.UserId;
 import com.edusalguero.rexoubador.domain.model.user.UserRepository;
+import com.edusalguero.rexoubador.domain.shared.Status;
 import com.edusalguero.rexoubador.domain.shared.ValidationException;
 import com.edusalguero.rexoubador.domain.shared.validator.UsernameUniqueValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-
 @Service
 public class UserRegistrationUseCase {
 
     private final UserRepository userRepository;
+    private final HarvesterRepository harvesterRepository;
     private final UsernameUniqueValidator usernameUniqueValidator;
 
     @Autowired
-    public UserRegistrationUseCase(UserRepository userRepository) {
+    public UserRegistrationUseCase(UserRepository userRepository, HarvesterRepository harvesterRepository) {
         this.userRepository = userRepository;
+        this.harvesterRepository = harvesterRepository;
         this.usernameUniqueValidator = new UsernameUniqueValidator(this.userRepository);
     }
 
     public UserId execute(String username, String password, String firstName, String lastName) {
         UserId uid = userRepository.nextIdentity();
 
-        if(!usernameUniqueValidator.validate(username))
-        {
+        if (!usernameUniqueValidator.validate(username)) {
             throw new ValidationException("Invalid username! Select a different one");
         }
         User u = new User(uid, username, password, firstName, lastName);
+        setupDefaultMonitors(u);
         userRepository.register(u);
         return uid;
     }
+
+    private void setupDefaultMonitors(User user) {
+        user.addHarvester(harvesterRepository.nextIdentity(), HarvesterType.DISK_USAGE,
+                "Disk usage", true, true,
+                "90", "95", Status.ENABLED);
+
+        user.addHarvester(harvesterRepository.nextIdentity(), HarvesterType.LOAD,
+                "CPU load", true, true,
+                "90", "95", Status.ENABLED);
+
+        user.addHarvester(harvesterRepository.nextIdentity(), HarvesterType.MEMORY_USAGE,
+                "Memory usage", true, true,
+                "90", "95", Status.ENABLED);
+
+    }
+
 
 }
